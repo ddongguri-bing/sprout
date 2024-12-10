18d97c9a24213f7a3c2210d25aa09388335ccd06
denshou <zecaumr@naver.com>
2024-12-10 22:25:33 +0900

    [FEAT] posting 로직 구현중

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const posting = async () => {
+  try {
+    const { data } = await axios.post(
+      `https://5th.fe.dev-cos.com:5004/posts/create`
+    );
+    console.log("posting 성공");
+    return data;
+  } catch (error) {}
+};
