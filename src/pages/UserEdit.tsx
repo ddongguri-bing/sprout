a504a1f92a29465cd11bb70ac7eea34cf0088128
sunhyeong <parksunhyeong529@gmail.com>
2024-12-11 17:40:56 +0900

    [FEAT]유저상세페이지 수정 중

--- src/pages/UserEdit.tsx
+++ src/pages/UserEdit.tsx
@@ -7,8 +7,11 @@ import Button from "../components/Button";
 import SettingInput from "../components/SettingInput";
 import { useAuthStore } from "../stores/authStore";
 import { useModal } from "../stores/modalStore";
+import { useState } from "react";
+import { postUploadPhoto } from "../api/users";
 
 export default function UserEdit() {
+  //로그아웃 관련
   const setOpen = useModal((state) => state.setModalOpen);
   const setModalOpts = useModal((state) => state.setModalOpts);
   const logout = useAuthStore((state) => state.logout);
@@ -34,6 +37,32 @@ export default function UserEdit() {
     });
   };
 
+  // 이미지 업로드 관련
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [photoUrl, setPhotoUrl] = useState("");
+  const [photoId, setPhotoId] = useState("");
+
+  const handleUploadPhoto = async (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      console.error("파일이 선택되지 않았습니다");
+      return;
+    }
+
+    setSelectedPhoto(file);
+
+    try {
+      const data = await postUploadPhoto({
+        isCover: false,
+        image: file,
+      });
+      setPhotoUrl(data.image);
+      setPhotoId(data._id);
+    } catch (error) {
+      console.log("이미지 업로드 실패", error);
+    }
+  };
+
   return (
     <>
       <div className="w-full h-[100px] px-[30px] mb-10 sticky top-0 left-0 flex justify-between items-center bg-white dark:bg-black dark:text-white border-b border-whiteDark dark:border-gray z-[9]">
@@ -43,8 +72,21 @@ export default function UserEdit() {
       </div>
       <form className="w-full max-w-[777px] mb-[125px] flex flex-col items-center mx-auto gap-[30px]">
         <label className="cursor-pointer relative mb-5">
-          <input type="file" accept="image/*" name="" id="" hidden />
-          <div className="w-[220px] h-[220px] bg-whiteDark rounded-[8px]"></div>
+          <input
+            type="file"
+            accept="image/*"
+            id={photoId}
+            hidden
+            onChange={handleUploadPhoto}
+          />
+          <div
+            className="w-[220px] h-[220px] bg-whiteDark rounded-[8px]"
+            style={
+              photoUrl
+                ? { background: `url(${photoUrl})`, backgroundSize: "cover" }
+                : {}
+            }
+          ></div>
           <span className=" absolute -bottom-[10px] -right-[10px] ">
             <img src={Camera} alt="camera icon" />
           </span>
