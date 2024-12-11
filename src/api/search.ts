f549ef45baad047349fee16d2ded75f50e24c4a6
junyoung <jadw9715@gmail.com>
2024-12-11 20:12:33 +0900

    [DOCS] search post type 명시

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,6 +1,27 @@
 import { axiosInstance } from ".";
 
-export const getSearchPosts = async (query?: string) => {
+export type PostItemType = {
+  likes: {
+    _id: string;
+    user: string;
+    post: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+  }[];
+  comments: string[];
+  _id: string;
+  title: string;
+  channel: string;
+  author: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
+
+export const getSearchPosts = async (
+  query?: string
+): Promise<PostItemType[]> => {
   return (await axiosInstance.get(`/search/all/${query}`)).data;
 };
 
