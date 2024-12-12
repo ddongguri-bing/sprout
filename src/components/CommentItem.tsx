e12474467cd5f06dd3a343f75a184cdd1ada2cb8
Kim, DongChan <kdc9050@naver.com>
2024-12-12 10:04:37 +0900

    [FEAT] 댓글 작성 추가

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router";
 import { Comment } from "../api/board";
 import Avata from "./Avata";
-
+import Close from "../assets/close.svg";
 interface CommentItemProps {
   comment: Comment;
 }
@@ -9,20 +9,25 @@ interface CommentItemProps {
 export default function CommentItem({ comment }: CommentItemProps) {
   console.log(comment);
   return (
-    <div className="flex gap-[10px] items-start">
-      <Link to={`/user/${comment.author._id}`}>
-        <Avata profile={comment.author.image} size={"sm"} />
-      </Link>
-      <div>
+    <div className="flex items-start justify-between gap-[10px]">
+      <div className="flex gap-[10px] items-start">
         <Link to={`/user/${comment.author._id}`}>
-          <h3 className="font-bold line-clamp-1 text-xs">
-            {comment.author.fullName}
-          </h3>
+          <Avata profile={comment.author.image} size={"sm"} />
         </Link>
-        <p className="text-gray dark:text-whiteDark text-sm">
-          {comment.comment}
-        </p>
+        <div>
+          <Link to={`/user/${comment.author._id}`}>
+            <h3 className="font-bold line-clamp-1 text-xs">
+              {comment.author.fullName}
+            </h3>
+          </Link>
+          <p className="text-gray dark:text-whiteDark text-sm">
+            {comment.comment}
+          </p>
+        </div>
       </div>
+      <button>
+        <img className="dark:invert w-3" src={Close} alt="close icon" />
+      </button>
     </div>
   );
 }
