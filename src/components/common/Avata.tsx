0f3675f9bf30419090b1f9a2cf497beb4bbb37b5
denshou <zecaumr@naver.com>
2024-12-13 17:02:44 +0900

    [STYLE] 스켈레톤 적용

--- src/components/common/Avata.tsx
+++ src/components/common/Avata.tsx
@@ -1,6 +1,8 @@
 import { twMerge } from "tailwind-merge";
 import { useTheme } from "../../stores/themeStore";
 import images from "../../constants/images";
+import { useState } from "react";
+import BoardImageSkeleton from "./skeleton/BoardImageSkeleton";
 export default function Avata({
   profile,
   size,
@@ -18,20 +20,41 @@ export default function Avata({
   }[size];
   const IMG_BASE_STYLE =
     "object-cover min-h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2";
+
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
   return (
     <div className={twMerge(BOX_BASE_STYLE, BOX_SIZE)}>
       {profile ? (
-        <img
-          src={profile}
-          alt="profile image"
-          className={twMerge(IMG_BASE_STYLE)}
-        />
+        <>
+          {!imageLoaded && <BoardImageSkeleton />}
+          <img
+            src={profile}
+            alt="profile image"
+            className={twMerge(
+              IMG_BASE_STYLE,
+              imageLoaded ? "opacity-100" : "opacity-0"
+            )}
+            onLoad={handleImageLoad}
+          />
+        </>
       ) : (
-        <img
-          src={isDark ? images.ProfileDark : images.Profile}
-          alt="profile image"
-          className={twMerge(IMG_BASE_STYLE, "w-[40%] min-h-0 ")}
-        />
+        <>
+          {!imageLoaded && <BoardImageSkeleton />}
+          <img
+            src={isDark ? images.ProfileDark : images.Profile}
+            alt="profile image"
+            className={twMerge(
+              IMG_BASE_STYLE,
+              "w-[40%] min-h-0",
+              imageLoaded ? "opacity-100" : "opacity-0"
+            )}
+            onLoad={handleImageLoad}
+          />
+        </>
       )}
     </div>
   );
