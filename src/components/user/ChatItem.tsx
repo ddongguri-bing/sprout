8c0f18c9b6384e0fe9234ef4ca69142010c0b146
sunhyeong <parksunhyeong529@gmail.com>
2024-12-16 09:23:15 +0900

    [FEAT]메시지 작업중

---
+++ src/components/user/ChatItem.tsx
@@ -0,0 +1,42 @@
+import Avata from "../common/Avata";
+import { useUserStore } from "../../stores/userStore";
+
+export default function ChatItem({
+  user,
+  msg,
+  onOpen,
+}: {
+  user: any;
+  msg: { message: string | undefined };
+  onOpen: () => void;
+}) {
+  const onlineUsers = useUserStore((state) => state.onlineUsers);
+  const isOnline = !!onlineUsers.find((ou) => ou._id === user._id);
+
+  return (
+    <>
+      <li
+        className={`w-full${msg?.message ? "cursor-pointer" : ""}`}
+        onClick={onOpen}
+      >
+        <div className="flex gap-[10px] items-center p-2 rounded-[8px] transition-all hover:bg-whiteDark/30">
+          <div className="relative">
+            <Avata profile={user.image} size={"sm"} />
+            {isOnline && (
+              <span className=" absolute -top-1 -right-1 flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-main/80 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-main"></span>
+              </span>
+            )}
+          </div>
+          <div className="text-xs  line-clamp-1">
+            <h3 className="font-bold line-clamp-1 text-black dark:text-white">
+              {user.fullName}
+            </h3>
+            <p className={"text-gray dark:text-whiteDark"}>{msg?.message}</p>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
--- src/components/user/SendMessage.tsx
+++ src/components/user/SendMessage.tsx
@@ -1,14 +1,28 @@
 import Button from "../common/Button";
 import TextareaAutosize from "react-textarea-autosize";
 
-export default function SendMessage({ onClose }: { onClose: () => void }) {
+export default function SendMessage({
+  onClose,
+  msgValue,
+  onMsgChange,
+  onSend,
+  receiver,
+}: {
+  onClose: () => void;
+  msgValue: string;
+  onMsgChange: (value: string) => void;
+  onSend: () => void;
+  receiver: string;
+}) {
   return (
     <form className="w-[calc(100%-32px)] max-w-[425px] h-[230px] p-5  bg-white dark:bg-grayDark rounded-[8px] flex flex-col items-center justify-center">
       <div className="font-bold mb-[30px] bg-white dark:bg-grayDark text-black dark:text-white w-full h-full border border-whiteDark dark:border-gray p-[10px] rounded-[8px]">
         <TextareaAutosize
           className="w-full h-[128px] focus:outline-none scroll resize-none bg-white dark:bg-grayDark font-normal"
-          placeholder="사용자님에게 보낼 메세지를 작성해주세요."
+          placeholder={`[${receiver}]님에게 보낼 메세지를 작성해주세요.`}
+          value={msgValue}
           maxRows={4}
+          onChange={(e) => onMsgChange(e.target.value)}
         />
       </div>
       <div className="flex items-center gap-5">
@@ -19,11 +33,7 @@ export default function SendMessage({ onClose }: { onClose: () => void }) {
           size={"sm"}
           theme="sub"
         />
-        <Button
-          // onClick={() => setOpen(false)}
-          text={"전송"}
-          size={"sm"}
-        />
+        <Button onClick={onSend} text={"전송"} size={"sm"} />
       </div>
     </form>
   );
