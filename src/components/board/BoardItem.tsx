cd68a7ff65e33e3bb5d81439c1d842c190bcfc26
denshou <zecaumr@naver.com>
2024-12-16 11:24:00 +0900

    link 제거

--- src/components/board/BoardItem.tsx
+++ src/components/board/BoardItem.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import images from "../../constants/images";
 import Comments from "./Comments";
 import { useState, useEffect } from "react";
