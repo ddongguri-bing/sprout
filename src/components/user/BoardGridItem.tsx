fb55f62a4f20821659827cc968164736698190e4
junyoung <jadw9715@gmail.com>
2024-12-19 16:04:07 +0900

    [STYLE] User Grid 사이즈

--- src/components/user/BoardGridItem.tsx
+++ src/components/user/BoardGridItem.tsx
@@ -18,7 +18,7 @@ export default function BoardGridItem({ post }: BoardGridItemProps) {
   return (
     <Link
       to={`/board/${post.channel}/${post._id}`}
-      className="aspect-square min-h-[50px] max-h-[280px] rounded-[8px] bg-white dark:bg-black overflow-hidden"
+      className="aspect-square min-h-[50px] md:max-h-[324px] max-h-[280px] rounded-[8px] bg-white dark:bg-black overflow-hidden"
     >
       {!JSON.parse(post.title).images.length ? (
         <div className="w-full h-full flex flex-col justify-center items-center border border-1 rounded-[8px] border-whiteDark dark:border-gray text-[14px] text-gray dark:text-whiteDark">
