1afc20ceb6b8896c40a2c0282468b49e118fdffa
ddongguri-bing <ckdalssnsk@naver.com>
2024-12-19 12:07:17 +0900

    [DESIGN] 유저 상세 페이지 반응형 스타일

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -220,7 +220,7 @@ export default function User() {
   };
   return (
     <>
-      <div className="h-[100px] px-[30px] z-[9] sticky top-0 left-0 flex justify-between items-center dark:text-white bg-white dark:bg-black border-b border-whiteDark dark:border-gray">
+      <div className="h-[100px] md:h-[65px] px-[30px] z-[9] sticky top-0 left-0 flex justify-between items-center dark:text-white bg-white dark:bg-black border-b border-whiteDark dark:border-gray">
         <button onClick={() => navigate(-1)}>
           <img
             className="dark:invert dark:hover:fill-white"
@@ -229,51 +229,49 @@ export default function User() {
           />
         </button>
       </div>
-      <div className="py-[70px] px-[50px] text-black dark:text-white flex flex-col items-center">
-        <div className="w-full max-w-[826px]">
-          <div className="flex mb-[30px] items-end gap-[30px]">
+      <div className="py-[70px] md:py-[30px] px-[50px] md:px-[20px] text-black dark:text-white flex flex-col items-center">
+        <div className="w-full max-w-[826px] md:max-w-full">
+          <div className="flex mb-[30px] md:mb-5 items-end gap-[30px]">
             <Avata
               profile={specificUser?.image}
               size="lg"
               onClick={handleProfileClick}
             />
-            <div className="flex flex-col gap-[50px]">
-              <div className="flex flex-col gap-[10px]">
-                <h2 className="text-[22px] font-bold">
+            <div className="flex flex-col gap-[50px] md:gap-[17px]">
+              <div className="flex flex-col gap-[10px] md:gap-0">
+                <h2 className="text-[22px] md:text-base font-bold">
                   {specificUser.fullName}
                 </h2>
-                <p className="text-lg text-gray dark:text-whiteDark">
+                <p className="text-lg md:text-xs text-gray dark:text-whiteDark">
                   {specificUser.email}
                 </p>
               </div>
               <div className="flex flex-col gap-[20px]">
                 <div className="flex gap-[30px]">
                   <div
-                    className="flex items-center gap-[10px] cursor-pointer"
+                    className="flex items-center gap-[10px] cursor-pointer md:flex-col md:gap-0"
                     onClick={() => {
                       setFollowListType("followers");
                       toggleFollowList();
-                    }}
-                  >
-                    <span className="">팔로우</span>{" "}
+                    }}>
+                    <span className="md:text-[14px]">팔로우</span>{" "}
                     <span className="text-gray dark:text-whiteDark font-bold">
                       {followerCount}
                     </span>
                   </div>
                   <div
-                    className="flex items-center gap-[10px] cursor-pointer"
+                    className="flex items-center gap-[10px] cursor-pointer md:flex-col md:gap-0"
                     onClick={() => {
                       setFollowListType("following");
                       toggleFollowList();
-                    }}
-                  >
-                    <span className="">팔로잉</span>{" "}
+                    }}>
+                    <span className="md:text-[14px]">팔로잉</span>{" "}
                     <span className="text-gray dark:text-whiteDark font-bold">
                       {specificUser.following.length}
                     </span>
                   </div>
-                  <div className="flex items-center gap-[10px]">
-                    <span>포스트</span>{" "}
+                  <div className="flex items-center gap-[10px] md:flex-col md:gap-0">
+                    <span className="md:text-[14px]">포스트</span>{" "}
                     <span className="text-gray dark:text-whiteDark font-bold ">
                       {specificUser.posts.length}
                     </span>
@@ -281,7 +279,7 @@ export default function User() {
                 </div>
 
                 {/* id가 내 id이면 프로필 수정 버튼 / 아니면 팔로잉 버튼 */}
-                <div className="flex gap-[30px] items-center">
+                <div className="flex gap-[30px] items-center md:hidden">
                   <Button
                     to={isMyPage ? `/user/edit` : undefined}
                     text={followBtnTxt}
@@ -292,8 +290,7 @@ export default function User() {
                   />
                   <button
                     type="button"
-                    onClick={() => handleClickMsg(isMyPage ? "CHAT" : "SEND")}
-                  >
+                    onClick={() => handleClickMsg(isMyPage ? "CHAT" : "SEND")}>
                     <img
                       className="w-[25px] h-[25px] dark:invert dark:hover:fill-white"
                       src={
@@ -304,6 +301,25 @@ export default function User() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="flex gap-[30px] items-center mb-[10px]">
+            <Button
+              to={isMyPage ? `/user/edit` : undefined}
+              text={followBtnTxt}
+              size={"md"}
+              className="max-w-[188px] hidden md:block"
+              onClick={handleClickFollow}
+              theme={followBtnTxt === "팔로우 끊기" ? "sub" : "main"}
+            />
+            <button
+              type="button"
+              className="hidden md:block"
+              onClick={() => handleClickMsg(isMyPage ? "CHAT" : "SEND")}>
+              <img
+                className="w-[25px] h-[25px] dark:invert dark:hover:fill-white hidden"
+                src={isMyPage ? images.MessageBoxBtn : images.MessageSendBtn}
+              />
+            </button>
           </div>
           <BoardGrid posts={specificUser.posts} />
         </div>
