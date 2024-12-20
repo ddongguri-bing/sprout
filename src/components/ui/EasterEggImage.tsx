b64b56f3a9800dd6667ab52f166e17d337f0e9dd
ddongguri-bing <ckdalssnsk@naver.com>
2024-12-20 12:05:44 +0900

    [REMOVE] 불필요한 import 삭제

--- src/components/ui/EasterEggImage.tsx
+++ src/components/ui/EasterEggImage.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Button from "../common/Button";
-import { FadeLoader } from "react-spinners";
 import easterEggs from "../../constants/easterEggs";
 import Loading from "../common/Loading";
 
