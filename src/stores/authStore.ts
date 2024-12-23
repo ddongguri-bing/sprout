e4412cd4832007e1c55048f7b77e8077f5d9e1fd
junyoung <jadw9715@gmail.com>
2024-12-23 14:46:07 +0900

    [FEAT] 토큰 쿠키 만료일자 추가

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -30,8 +30,8 @@ export const useAuthStore = create(
         set({ isLoggedIn: false, token: null, user: null, isSocial: false }),
     }),
     {
-      name: "food-storage", // name of the item in the storage (must be unique)
-      storage: createJSONStorage(() => sessionStorage), // (optional)이기 때문에 해당 줄을 적지 않으면 'localStorage'가 기본 저장소로 사용된다.
+      name: "auth-storage",
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
 );
