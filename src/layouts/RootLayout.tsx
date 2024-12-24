bf087993e8df2e76c724fe82691b7ea5f96f05aa
junyoung <jadw9715@gmail.com>
2024-12-24 09:59:44 +0900

    [FEAT] theme 보완

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -9,17 +9,14 @@ import { Outlet } from "react-router";
 export default function RootLayout() {
   const [loading, setLoading] = useState<boolean>(true);
   // 다크모드 고정
-  const setIsDarkMode = useTheme((state) => state.setIsDarkMode);
+  const isDarkMode = useTheme((state) => state.isDarkMode);
   // auth 상태값 확인
   const login = useAuthStore((state) => state.login);
   const logout = useAuthStore((state) => state.logout);
   const [cookies, _, removeCookie] = useCookies(["token"]);
 
   useEffect(() => {
-    if (localStorage.getItem("theme") === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark"); // 처음 페이지 로드 시 'dark' 클래스를 추가
-    }
+    if (isDarkMode) document.documentElement.classList.add("dark"); // 처음 페이지 로드 시 'dark' 클래스를 추가
     const { token } = cookies;
     const handleGetUser = async (token: string) => {
       try {
--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ThemeStore {
   isDarkMode: boolean;
   setIsDarkMode: (checked: boolean) => void;
 }
 
-export const useTheme = create<ThemeStore>((set) => ({
-  isDarkMode: false,
-  setIsDarkMode: (checked: boolean) => set(() => ({ isDarkMode: checked })),
-}));
+export const useTheme = create(
+  persist<ThemeStore>(
+    (set) => ({
+      isDarkMode: false,
+      setIsDarkMode: (checked: boolean) => set(() => ({ isDarkMode: checked })),
+    }),
+    {
+      name: "theme-storage",
+    }
+  )
+);
